b7d1ffc952ee34d3c2b95322060ed3a9271eccba
belak0101 <belak0101@gmail.com>
2025-02-27 19:49:54 +0000

    Updated to events page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Las Vegas East Side Community Events";
+export const SITE_TITLE = "LV East Community Events";
 export const SITE_DESCRIPTION = "Welcome to Las Vegas East Side Community Events";
---
+++ src/content/blog/2025-Easter.md
@@ -0,0 +1,8 @@
+---
+title: "2025 Community Easter Event"
+description: "Join us for an Easter Event open to the community"
+pubDate: "Feb 27 2025"
+heroImage: "/2025EasterEvent.png"
+---
+
+Join us for an Easter Event. 
